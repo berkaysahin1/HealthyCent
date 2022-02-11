1ee3cb16e407b24f54763883f3bc8b3e4a7612f1
Berkay Şahin <berkays2015@gmail.com>
2022-02-11 20:22:13 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/healthycent/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.healthycent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:healthycent/util/places.dart';
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          // IconButton(
+          //   icon: IconBadge(
+          //     icon: Icons.notifications_none,
+          //   ),
+          //   onPressed: () {},
+          // ),
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              "Where are you \ngoing?",
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            // child: SearchBar(),
+          ),
+
+        ],
+      ),
+    );
+  }
+
+
+}
